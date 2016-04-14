b1d4072e6e92b0b9ad5f404f4abc6dea24b3ea55
amalgam-silver <amalgam_silver@126.com>
2016-04-14 23:35:45 +0800

    just some ui
    just some ui

--- chapter3-cds/cds.lisp
+++ chapter3-cds/cds.lisp
@@ -20,11 +20,11 @@
    (prompt-read "Title")
    (prompt-read "Artist")
    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
-   (y-or-n-p "Ripped [y/n]:")))
+   (y-or-n-p "Ripped?")))
 
 (defun add-cds ()
   (loop (add-record (prompt-for-cd))
-     (if (not (y-or-n-p "Another? [y/n]")) (return))))
+     (if (not (y-or-n-p "Another?")) (return))))
 
 (defun save-db (filename)
   (with-open-file (out filename
